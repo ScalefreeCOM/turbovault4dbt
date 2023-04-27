51d2650fa3ca2f23aa518d239c3f0a6f52133363
hoale1212 <hle@scalefree.com>
2023-04-27 10:25:36 +0200

    Adjusted Python Scripts for other sources

--- turbovault_google-sheets.py
+++ turbovault_google-sheets.py
@@ -6,6 +6,10 @@ from procs.sqlite3 import hub
 from procs.sqlite3 import link
 from procs.sqlite3 import pit
 from procs.sqlite3 import nh_satellite
+from procs.sqlite3 import ma_satellite
+from procs.sqlite3 import nh_link
+from procs.sqlite3 import sources
+from procs.sqlite3 import tests
 import pandas as pd
 import gspread as gs
 import sqlite3
@@ -63,41 +67,58 @@ def main():
     generated_timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
 
     parser = GooeyParser(description='Config')
-    parser.add_argument("--Tasks",help="Select the entities which You want to generate",action="append",widget='Listbox',choices=['Stage','Hub','Satellite','Link','Pit','Non Historized Satellite'],default=['Stage','Hub','Satellite','Link','Pit','Non Historized Satellite'],nargs='*',gooey_options={'height': 300})
+    parser.add_argument("--Tasks",help="Select the entities which You want to generate",action="append",widget='Listbox',choices=['Stage','Standard Hub','Standard Satellite','Standard Link','Non Historized Link','Pit','Non Historized Satellite','Multi Active Satellite'],default=['Stage','Standard Hub','Standard Satellite','Standard Link','Non Historized Link','Pit','Non Historized Satellite','Multi Active Satellite'],nargs='*',gooey_options={'height': 300})
     parser.add_argument("--Sources",action="append",nargs="+", widget='Listbox', choices=available_sources, gooey_options={'height': 300},
-                       help="Select the sources which You want to process")
+                       help="Select the sources which You want to process", default=[])
+    parser.add_argument("--SourceYML",default=False,action="store_true",  help="Do You want to generate the sources.yml?") #Create external Table (Y/N)
+    parser.add_argument("--Test",default=False,action="store_true",  help="Do You want to generate YML-Files for Standard DataVault tests?") #Create external Table (Y/N)
+
     args = parser.parse_args()
-   
+
     try:
-        todo = args.Tasks[6]
+        todo = args.Tasks[8]
     except IndexError:
         print("Keine Entitäten ausgesucht.")
         todo = ""     
 
-    rdv_default_schema = sheet_url = config.get('Google Sheets', 'rdv_schema')
-    stage_default_schema = sheet_url = config.get('Google Sheets', 'stage_schema')
+    rdv_default_schema = config.get('Excel',"rdv_schema")
+    stage_default_schema = config.get('Excel',"stage_schema")
 
-   
-    for source in args.Sources[0]:
-        if 'Stage' in todo:
-            stage.generate_stage(cursor,source, generated_timestamp, stage_default_schema, model_path, hashdiff_naming)
+    if args.SourceYML:
+        sources.gen_sources(cursor,generated_timestamp,model_path)
+
+
+
+    try:
+        for source in args.Sources[0]:
+            if args.Test:
+                tests.gen_tests(cursor,source,generated_timestamp,model_path)
+            if 'Stage' in todo:
+                stage.generate_stage(cursor,source, generated_timestamp, stage_default_schema, model_path, hashdiff_naming)
+            
+            if 'Standard Hub' in todo: 
+                hub.generate_hub(cursor,source, generated_timestamp, rdv_default_schema, model_path)
         
-        if 'Hub' in todo: 
-            hub.generate_hub(cursor,source, generated_timestamp, rdv_default_schema, model_path)
-    
-        if 'Link' in todo: 
-            link.generate_link(cursor,source, generated_timestamp, rdv_default_schema, model_path)
+            if 'Standard Link' in todo: 
+                link.generate_link(cursor,source, generated_timestamp, rdv_default_schema, model_path)
 
-        if 'Satellite' in todo: 
-            satellite.generate_satellite(cursor, source, generated_timestamp, rdv_default_schema, model_path, hashdiff_naming)
+            if 'Standard Satellite' in todo: 
+                satellite.generate_satellite(cursor, source, generated_timestamp, rdv_default_schema, model_path, hashdiff_naming)
+                
+            if 'Pit' in todo:
+                pit.generate_pit(cursor,source, generated_timestamp, model_path)
+                
+            if 'Non Historized Satellite' in todo: 
+                nh_satellite.generate_nh_satellite(cursor, source, generated_timestamp, rdv_default_schema, model_path)
+                
+            if 'Multi Active Satellite' in todo: 
+                ma_satellite.generate_ma_satellite(cursor, source, generated_timestamp, rdv_default_schema, model_path, hashdiff_naming)
             
-        if 'Pit' in todo:
-            pit.generate_pit(cursor,source, generated_timestamp, model_path)
-            
-        if 'Non Historized Satellite' in todo: 
-            nh_satellite.generate_nh_satellite(cursor, source, generated_timestamp, rdv_default_schema, model_path)
-   
+            if 'Non Historized Link' in todo:
+                nh_link.generate_nh_link(cursor,source, generated_timestamp, rdv_default_schema, model_path)
 
+    except IndexError:
+        print("Keine Quelle ausgesucht.")
 if __name__ == "__main__":
     print("Starting Script.")
     start = time.time()
